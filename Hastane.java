4ba6fa77d57f06830bcdc4818980e7a9681ca489
fersatir <fatihersatir@gmail.com>
2022-04-15 10:36:17 +0300

    son version

--- Hastane.java
+++ Hastane.java
@@ -1,13 +1,40 @@
 package projects.hastaneDeneme;
 
-public class Hastane {
-    public static void main(String[] args) {
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
+import Projects.P08_HastaneOtomasyon_.Doktor;
+import Projects.P08_HastaneOtomasyon_.Durum;
+import Projects.P08_HastaneOtomasyon_.Hasta;
+import Projects.P08_HastaneOtomasyon_.VeriBankasi;
+
+public class Hastane extends VeriBankasi {
+
+    private Doktor doktor;
+    private Hasta hasta;
+    private Durum durum;
+
+    public Durum getDurum() {
+        return durum;
     }
+
+    public void setDurum(Durum durum) {
+        this.durum = durum;
+    }
+
+    public Doktor getDoktor() {
+        return doktor;
+    }
+
+    public void setDoktor(Doktor doktor) {
+        this.doktor = doktor;
+    }
+
+    public Hasta getHasta() {
+        return hasta;
+    }
+
+    public void setHasta(Hasta hasta) {
+        HastaAdiSoyadi.add(hasta.getHastaAdiSoyadi());
+        HastaId.add(hasta.getHastaId());
+        this.hasta = hasta;
+    }
+
 }
