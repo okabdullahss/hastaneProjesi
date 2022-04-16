a95651735cb9d7b8ef32f282c2395939c06645ac
BetulUys <100969641+BetulUys@users.noreply.github.com>
2022-04-17 02:28:20 +0300

    Add files via upload
    BetulUys V1

--- Doktor.java
+++ Doktor.java
@@ -1,4 +1,45 @@
-package projects.hastaneDeneme;
+package Hastane_Projesi;
 
 public class Doktor {
+    private String isim;
+    private String soyIsim;
+    private String unvan;
+
+    Doktor() {
+    }
+
+
+    @Override
+    public String toString() {
+        return "Doktor{" +
+                "isim='" + isim + '\'' +
+                ", soyIsim='" + soyIsim + '\'' +
+                ", unvan='" + unvan + '\'' +
+                '}';
+    }
+
+    public String getIsim() {
+        return isim;
+    }
+
+    public void setIsim(String isim) {
+        this.isim = isim;
+    }
+
+    public String getSoyIsim() {
+        return soyIsim;
+    }
+
+    public void setSoyIsim(String soyIsim) {
+        this.soyIsim = soyIsim;
+    }
+
+    public String getUnvan() {
+        return unvan;
+    }
+
+    public void setUnvan(String unvan) {
+        this.unvan = unvan;
+    }
+
 }
